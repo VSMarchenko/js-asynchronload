3efd69b6eab7516b9658b1b6adcc5d8fc20c6994
Viacheslav Marchenko <vsmarchenko@gmail.com>
2017-11-16 11:21:14 +0100

    Update jsasynchronload.js

--- jsasynchronload.js
+++ jsasynchronload.js
@@ -1,81 +1,91 @@
 (function($){
-    jQuery.prototype.jsasynchronload = function(options){
-		options = $.extend({
-			thisClass:'item',
-            preloaderImage:'https://martphoto.com.ua/wp-content/themes/martphoto/img/preloaderImg.gif',
-            blockColors:['223, 58, 62', '72, 193, 196'],    
-            after:function(){return true;}
+    jQuery.prototype.jssynchronization = function(options){
+	options = $.extend({
+		thisClass:'item',
+		preloaderImage:'https://martphoto.com.ua/wp-content/themes/martphoto/img/preloaderImg.gif',
+		blockColors:['223, 58, 62', '72, 193, 196'],    
+		after:function(){return true;}
         }, options);
         
         var that = $(this);
-		var thisSelector = this.selector;
+	var thisSelector = this.selector;
         var thisClass = thisSelector + " ." + options.thisClass;
-		var total = $(thisClass+" img").length;
-        $(that).parent().css({background:'url('+options.preloaderImage+') no-repeat center center'});
+	var total = $(thisClass+" img").length;
         var countColor = options.blockColors.length;
+        $(that).parent().css({background:'url('+options.preloaderImage+') no-repeat center center'});
         
         $(thisClass+" img").each(function(i){
             $(this).hide();
+	    // Ховаємо каритнку в блоці й заповнюємо сам блок кольорами з опції options.blockColors
             $(this).parent().css({background:'rgba('+options.blockColors[Math.round( Math.random(0,(countColor - 1)) )]+', '+Math.random(5,10)+') url('+options.preloaderImage+') no-repeat center center'});
-        });
+        });	
 		
-        var init = function(){
-            var t;
-            var h = new Array();
+	var synchronize = function(){
             var key = false; // Ключ чекає заватаження всіх файлів
-            var img = new Array();
-            
-            function heightImg(){
-                $(thisClass+" img").each(function(i){
-                    h[i] = $(this).height();                    
-                })
-            }
-            
-            function imgLoaded(images) {                         
+            var img = [];
+            var h = [];
+		
+            var imgLoaded = function(images) {                         
                 images.parent().css({background:'none'});
                 images.animate({opacity:1},500);
-				setTimeout(function(){
-					options.after()
-				},200)
-            }
-            
-            heightImg();
-            
-            $(thisClass+" img").each(function(i){
-                img[i] = $(this);
-            })
-                        
-            t = setInterval(function(){
-                for(var i = 0; i < total; i++) {
-                    if (h[i] === 0) {
+		setTimeout(function(){
+			options.after();
+		},200);
+            };
+			
+		var heightImg = function(){
+			$(thisClass+" img").each(function(i){
+				h[i] = $(this).height();                    
+			});
+		};
+
+		// Визначаємо 
+		this.imgList = function(){
+			$(thisClass+" img").each(function(i){
+				img[i] = $(this);
+			});
+		};
+
+		this.start= function(){
+			// Перевіряємо чи почала картинка завантажуватися, функція оновлюється серез заданий проміжок часу. Коли всі картинки завантажаться, функція зупиниться
+			var t = setInterval(function(){
+				heightImg();
+				for(var i = 0; i < total; i++) {
+					if (h[i] === 0) {
 						key = false;
 						break;
 					}else{
 						key = true;	
 					}
-                }
-                            
-                if(key){
-                    options.after();
-                    $(thisClass+" img").show().css({opacity:0});
-                    $(that).parent().css({background:'none'});
-                    
-                    for(var i = 0; i < img.length; i++){
-                        var image = new Image();
-                        image.onload = imgLoaded.bind(this, img[i]);
-                        image.src = img[i].attr('src');
-						
+				}
+				if(key){
+					options.after();
+					$(thisClass+" img").show().css({opacity:0});
+					$(that).parent().css({background:'none'});
+
+					for(var i = 0; i < img.length; i++){
+						var image = new Image();
+						image.onload = imgLoaded.bind(this, img[i]);
+						image.src = img[i].attr('src');
 						if (image.complete){
-                            setTimeout(function(){
+							setTimeout(function(){
 								imgLoaded.bind(this, img[i]);
 							},200);
-                        }
-                    }
-                    clearInterval(t);                    
-                }
-                heightImg();
-            },10);
+						}
+					}
+					clearInterval(t);                    
+				}
+			},10);
+
+		};
         };
+		
+        var init = function(){
+		var js = new synchronize();
+		js.imgList();
+		js.start();
+        };
+	    
         return this.each(init);       
     };
 })(jQuery);
